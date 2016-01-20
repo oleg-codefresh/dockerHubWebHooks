625f75ae6f0624040640ab2e93371ba795518c5b
Oleg verhovsky <verchol@gmail.com>
2016-01-20 14:36:48 +0200

    add docker

--- runImage.sh
+++ runImage.sh
@@ -1,3 +1,4 @@
 docker pull buildup/backend:test
+export PATH=$PATH:/opt/local/bin
 docker rm -fv $(docker ps  | grep build | awk '{print $1}')
 docker run --name buildup -p 1111:80 buildup/backend:test
