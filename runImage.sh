0638ea8fc0c471460c0413a82a99ac8433a6b240
Sergey Bahchissaraitsev <sergey@bahchis.com>
2016-01-20 15:07:32 +0200

    contaner name backend

--- runImage.sh
+++ runImage.sh
@@ -1,4 +1,4 @@
 docker pull buildup/backend:test
 export PATH=$PATH:/opt/local/bin
-docker rm -fv $(docker ps  | grep build | awk '{print $1}')
-docker run -d -h buildup --name buildup -p ${HOST_HTTPS_PORT}:443 -p ${HOST_HTTP_PORT}:80 buildup/backend:test
+docker rm -fv $(docker ps  | grep buildup-backend | awk '{print $1}')
+docker run -d -h buildup-backend --name buildup-backend -p ${HOST_HTTPS_PORT}:443 -p ${HOST_HTTP_PORT}:80 buildup/backend:test
