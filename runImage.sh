87950001aa688a2cbff7a8b45302dda11a4f8630
Sergey Bahchissaraitsev <sergey@bahchis.com>
2016-01-20 12:46:59 +0200

    run image port adjustments

--- runImage.sh
+++ runImage.sh
@@ -1,3 +1,3 @@
 docker pull buildup/backend:test
 docker rm -fv $(docker ps  | grep build | awk '{print $1}')
-docker run --name buildup -p 1111:80 buildup/backend:test
+docker run -d -h buildup --name buildup -p ${HOST_HTTPS_PORT}:443 -p ${HOST_HTTP_PORT}:80 buildup/backend:test
