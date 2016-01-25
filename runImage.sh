50dd466d72bb735207e0203792ae8689231ea4ee
Oleg verhovsky <verchol@gmail.com>
2016-01-25 21:09:09 +0200

    fix

--- runImage.sh
+++ runImage.sh
@@ -1,7 +1,9 @@
 
 docker rm -fv $(docker ps  | grep buildup/backend | awk '{print $1}')
-docker rmi -f $(docker images | grep buildup/backend| awk '{print $3}')
+#docker rmi -f $(docker images | grep buildup/backend| awk '{print $3}')
 docker pull buildup/backend:test
 export PATH=$PATH:/opt/local/bin
 #docker rm -fv $(docker ps  | grep build | awk '{print $1}')
+export HOST_HTTPS_PORT=4433
+export HOST_HTTP_PORT=8888
 docker run -d -h buildup --name buildup -p ${HOST_HTTPS_PORT}:443 -p ${HOST_HTTP_PORT}:80 buildup/backend:test
