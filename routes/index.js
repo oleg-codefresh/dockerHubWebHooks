bad1348697faeb66014b1a03be2674ad57e4113d
Oleg verhovsky <verchol@gmail.com>
2016-01-25 20:30:53 +0200

    update deploy script

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-console.log(process.cwd);
+var webhooks = [];
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/log', function(req, res ,next){
+   res.send(JSON.stringify(webhooks));
 });
 /* GET home page. */
 router.post('/exit', function(req, res, next) {
@@ -14,6 +18,7 @@ router.post('/dockerhub', function(req, res, next) {
   //console.log(JSON.stringify(req));
   var runScript = require('./runScript');
   runScript(function(){
+      webhooks.push(req.body);
       console.log('hook was handled');
   });
   res.send('hook was handled');
--- runImage.sh
+++ runImage.sh
@@ -1,4 +1,7 @@
+
+docker rm -fv $(docker ps  | grep build | awk '{print $1}')
+docker rmi -f $(docker images | grep buildup/backend| awk '{print $3}')
 docker pull buildup/backend:test
 export PATH=$PATH:/opt/local/bin
-docker rm -fv $(docker ps  | grep build | awk '{print $1}')
+#docker rm -fv $(docker ps  | grep build | awk '{print $1}')
 docker run -d -h buildup --name buildup -p ${HOST_HTTPS_PORT}:443 -p ${HOST_HTTP_PORT}:80 buildup/backend:test
