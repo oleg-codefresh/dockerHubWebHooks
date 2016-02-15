2090808f50bb1df5a8c396f98498d9a3d04c081d
Oleg verhovsky <verchol@gmail.com>
2016-02-15 10:21:22 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -214,6 +214,6 @@ gulp.task('build', ['clean:dist'], function () {
   runSequence(['images', 'copy:extras', 'copy:fonts', 'client:build']);
 });
 
-//runapp
+//runapp 
 
 gulp.task('default', ['runapp']);
